0d017b169260f701a718a9614b42fc3ceb2f888b
Kiara <kiara_akiamane@hotmail.com>
2026-05-22 00:05:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const sectionFirstScreen = document.getElementById('fisrtscreensection');
+const passwordScreen = document.getElementById('passwordscreen')
+
+setTimeout(() => {
+    passwordScreen.classList.add('fade-in');
+    setTimeout(() => {
+        sectionFirstScreen.classList.add('fade-out');
+    }, 100);
+}, 5000)
